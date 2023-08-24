e5a0bc74a8120b5baf35c07ca2101e15bfec6d67
yaron <yaron@tivon.ort.org.il>
2023-08-24 13:34:19 +0300

    read nums

---
+++ input-outputs/ReadNums/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ReadNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.WriteLine("Enter two numbers:");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
