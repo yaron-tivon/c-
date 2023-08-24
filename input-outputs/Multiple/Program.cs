09299f494a88bad43ed1906a8713a213e45fa3d8
yaron <yaron@tivon.ort.org.il>
2023-08-24 13:57:18 +0300

    Multiple

---
+++ input-outputs/Multiple/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Multiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+            Console.WriteLine("Enter two numbers:");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine($"{num1}*{num2}={num1*num2}");
+        }
+    }
+}
